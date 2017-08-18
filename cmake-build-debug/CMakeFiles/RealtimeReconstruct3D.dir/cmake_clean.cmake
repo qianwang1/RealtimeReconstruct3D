6457159687b8b7a3b459294a4da1d96d37e22477
troll__zhao <troll__zhao@163.com>
2017-08-18 17:16:28 +0800

    initialize

---
+++ cmake-build-debug/CMakeFiles/RealtimeReconstruct3D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RealtimeReconstruct3D.dir/main.cpp.o"
+  "RealtimeReconstruct3D.pdb"
+  "RealtimeReconstruct3D"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RealtimeReconstruct3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
